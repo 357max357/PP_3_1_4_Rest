315357a66bd895a2c2be15ed1b2a382b74f603d2
MaximAntsiferov357 <maxim.a@wildnorth.games>
2023-03-06 20:19:14 +0300

    One-time parameters have been removed

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
@@ -19,9 +18,7 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String getAdminPage(Principal principal, ModelMap model) {
-        User user = userService.findByEmail(principal.getName());
-        model.addAttribute("user", user);
-
+        model.addAttribute("user", userService.findByEmail(principal.getName()));
         return "admin";
     }
 }
